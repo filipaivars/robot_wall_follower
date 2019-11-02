9e128daa75ba6e6b25813a42c36d0f55de662240
filipaivars <ivars.filipa@gmail.com>
2019-11-02 20:58:35 +0000

    add config files

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/filipaivars/Documents/Projects/robot_wall_follower/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/filipaivars/Documents/Projects/robot_wall_follower/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/filipaivars/Documents/Projects/robot_wall_follower/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/filipaivars/Documents/Projects/robot_wall_follower/catkin_ws/src:$ROS_PACKAGE_PATH"
